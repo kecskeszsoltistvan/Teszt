91e5c68c0d2f0980bae28cecd79b9e74dd8ea357
kecskeszsoltistvan <78853814+kecskeszsoltistvan@users.noreply.github.com>
2022-03-09 13:23:45 +0100

    updated ratio 🗿

--- hangman/teszt.py
+++ hangman/teszt.py
@@ -1,14 +1,20 @@
+import requests
 from tkinter import *
+
+req = requests.get("https://api.ipify.org?format=json")
+ip = req.json()["ip"]
+print(ip)
+
 ablak1 = Tk()
 ablak1.title("IKT Projekt - Zoltán nap")
-ablak1.geometry('450x500')
+ablak1.geometry('500x700')
 icon = PhotoImage(file = "C:\\Users\\kecskeszsoltistvan\\Downloads\\hangman\\favicon.png")
-ablak1.config(background="black")
+ablak1.config(background="white")
 ablak1.iconphoto(True, icon)
 kep1 = PhotoImage(file = "C:\\Users\\kecskeszsoltistvan\\Downloads\\hangman\\xy.gif")
 
 cimke = Label(ablak1, 
-        text="25.30.242.124", 
+        text=ip, 
         fg="#000000",
         font=("Comic Sans MS", 35, "bold"),
         bd=10,
@@ -16,5 +22,17 @@ cimke = Label(ablak1,
         pady=10,
         image = kep1,
         compound="bottom").pack()
-
+gomb1 = Button(ablak1,
+            text= "Ratio 🗿",
+            bg= "#ffffff",
+            fg= "#bbbbbb",
+            font= ("Impact", 10),
+            bd=10,
+            padx = 5,
+            pady = 2,
+            compound = "left",
+            activebackground = "#ffffff",
+            activeforeground = "#ffffff",
+            relief = "flat")
+gomb1.pack(pady = 20)
 ablak1.mainloop()
